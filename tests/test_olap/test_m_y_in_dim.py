16aeff9f28e5dc6d1c9c7a3d1b00d6c8cd8cf7e9
konstantin_s <konstantin.suspitsyn@gmail.com>
2025-03-24 22:56:59 +0300

    Fixed wrong column name in where
    Took 37 minutes

--- tests/test_olap/test_m_y_in_dim.py
+++ tests/test_olap/test_m_y_in_dim.py
@@ -4,7 +4,7 @@ from comradewolf.universe.olap_service import OlapService
 from comradewolf.universe.olap_structure_generator import OlapStructureGenerator
 from comradewolf.utils.olap_data_types import OlapFrontend, OlapFrontendToBackend
 from tests.constants_for_testing import get_olap_sales_folder
-from tests.test_olap.test_frontend_data import sales_olap_date_and_pcs, sales_olap_distinct
+from tests.test_olap.test_frontend_data import sales_olap_date_and_pcs, sales_olap_distinct, sales_olap_and_pcs_where
 
 olap_structure_generator: OlapStructureGenerator = OlapStructureGenerator(get_olap_sales_folder())
 olap_select_builder = OlapPostgresSelectBuilder()
@@ -33,8 +33,14 @@ def test_count_distinct() -> None:
     s = olap_service.select_data(frontend_to_backend_type, olap_structure_generator.get_tables_collection())
     print(s)
 
+def test_correct_where() -> None:
+    frontend_to_backend_type: OlapFrontendToBackend = olap_prompt_service.create_frontend_to_backend(
+        sales_olap_and_pcs_where, frontend_all_items_view)
 
+    s = olap_service.select_data(frontend_to_backend_type, olap_structure_generator.get_tables_collection())
+    assert "week_f" in s['sales.sales.date_sku']['sql']
 
 if __name__ == "__main__":
     # test_date_in_dimension()
-    test_count_distinct()
+    # test_count_distinct()
+    test_correct_where()
